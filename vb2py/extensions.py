6f52e1beed1f6a7ea7049442cfe917e1d2690c5c
Paul Paterson <ppaterson@gmail.com>
2019-03-02 17:08:15 -0600

    Removed code that caused Plugin pyc files to be created in the wrong place

--- vb2py/extensions.py
+++ vb2py/extensions.py
@@ -20,10 +20,11 @@ def loadAllPlugins():
     for mod in plugins.mods:
         log.info("Checking '%s' for plugins" % mod)
         #
-        f = open(os.path.join(rootPath(), "plugins", "%s.py" % mod), "r")
+        filename = os.path.join(rootPath(), "plugins", "%s.py" % mod)
+        f = open(filename, "r")
         try:
             try:
-                m = imp.load_module(mod, f, "Plugin-%s" % mod, ('*.py', 'r', 1))
+                m = imp.load_module(mod, f, filename, ('*.py', 'r', 1))
             finally:
                 f.close()
         except Exception, err:
