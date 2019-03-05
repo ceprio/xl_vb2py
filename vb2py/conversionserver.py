86a693fc63c1c01a13a7e774b875f40ccf27870f
Paul Paterson <ppaterson@gmail.com>
2019-03-04 22:14:12 -0600

    Added logging and allow form module

--- vb2py/conversionserver.py
+++ vb2py/conversionserver.py
@@ -6,14 +6,37 @@ import config
 import json
 from flask import Flask, request
 from flask_cors import CORS
+import time
 
 
 class ConversionError(Exception): """There was an error converting a line of text"""
 
 
+#
+# Logging config
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['wsgi']
+    }
+})
+
+
 app = Flask('VB2PY')
 CORS(app)
 Config = config.VB2PYConfig()
+app.logger.info('Starting conversion server')
 
 
 class ConversionHandler(object):
@@ -68,21 +91,27 @@ def testResult():
 @app.route('/single_code_module', methods=['POST'])
 def singleCodeModule():
     """Return a code module converted"""
+    app.logger.info('Code module conversion')
     return singleModule(parserclasses.VBModule())
 
 
 @app.route('/single_class_module', methods=['POST'])
 def singleClassModule():
     """Return a class module converted"""
+    app.logger.info('Class module conversion')
     return singleModule(parserclasses.VBClassModule())
+
+
+@app.route('/single_form_module', methods=['POST'])
+def singleFormModule():
+    """Return a form module converted"""
+    app.logger.info('Form module conversion')
+    return singleModule(parserclasses.VBFormModule())
 
 
 def singleModule(module_type):
     """Convert a single module"""
-    #
-    # Set the type
-    configuration = config.VB2PYConfig()
-
+    start_time = time.time()
     try:
         text = request.values['text']
         conversion_style = request.values['style']
@@ -90,6 +119,9 @@ def singleModule(module_type):
         result = 'No text or style parameter passed'
         status = 'FAILED'
     else:
+        app.logger.info('Starting conversion (%s) - %d lines' % (
+            conversion_style, len(text),
+        ))
         try:
             result = ConversionHandler.convertSingleFile(
                 text,
@@ -97,9 +129,11 @@ def singleModule(module_type):
                 conversion_style,
             )
             status = 'OK'
-        except ConversionError, err:
+        except Exception, err:
             result = str(err)
             status = 'ERROR'
+    #
+    app.logger.info('Completed with status %s. Time took %5.2fs' % (status, time.time() - start_time))
     #
     return json.dumps({
         'status': status,
