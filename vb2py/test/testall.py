c994a263c17489a1c0874e11b7e77c549b9c2999
Paul Paterson <ppaterson@gmail.com>
2020-04-05 12:38:12 -0500

    Cleaned up testall output to be more usable

--- vb2py/test/testall.py
+++ vb2py/test/testall.py
@@ -1,6 +1,5 @@
 import glob
 import os
-import unittest
 import sys
 import re
 import time
@@ -14,10 +13,41 @@ if len(sys.argv) == 2:
     if sys.argv[1] == "-v":
         show_errors = 1
 
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def preferentialSort(original, preferred_items):
+    """Move the preferred items to the start of the original list
+
+    Used to put the failing tests first so that we can quickly see them.
+
+    """
+    for item in preferred_items:
+        try:
+            idx = original.index(item)
+        except ValueError:
+            pass
+        else:
+            original.pop(idx)
+            original.insert(0, item)
+
+
 if __name__ == "__main__":
     print "\nStarting testall at %s\n" % time.ctime()
+    #
     files = glob.glob(r"test/test*.py")
     files.sort()
+    preferentialSort(files, ['test/testdotnet.py', 'test/testdotnet_execution.py', 'test/testfailures.py'])
+    #
     total_run = 0
     total_failed = 0
     start = time.time()
@@ -25,10 +55,16 @@ if __name__ == "__main__":
         for file in files:
             if file not in (r"test/testall.py", r"test/testframework.py", "test/testparser.py"):
                 fname = os.path.join((r"python %s" % vb2py.utils.rootPath()), file)
-                print "Running '%s' ... " % file,
+                print ((bcolors.ENDC + "Running '%s' " % file) + bcolors.OKGREEN).ljust(55, '.'),
+                #
+                start_time = time.time()
                 pi, po, pe = os.popen3(fname)
                 result = pe.read()
-                if result.find("FAILED") > -1:
+                duration = time.time() - start_time
+                #
+                print '[%5.1fs]' % duration,
+                #
+                if result.find("FAILED ") > -1:
                     try:
                         num = int(fail.match(result).groups()[0])
                         num_fail = int(fail.match(result).groups()[1])
@@ -37,20 +73,21 @@ if __name__ == "__main__":
                     total_run += num
                     total_failed += num_fail
                     if show_errors:
-                                    print "\n%s" % result
+                        print bcolors.OKBLUE + "\n%s" % result
                     else:
-                        print "*** %s errors out of %s" % (num_fail, num)
+                        print bcolors.FAIL + bcolors.BOLD + "*** %s errors out of %s" % (num_fail, num)
                 else:
                     try:
                         num = int(ok.match(result).groups()[0])
                     except:
-                        print "Failed completely: %s" % result
+                        print bcolors.FAIL + bcolors.BOLD + "Failed completely: %s" % result
                     else:
-                        print "Passed %s tests" % num
+                        print bcolors.OKBLUE + "Passed %s tests" % num
                         total_run += num
                 pi.close()
                 po.close()
                 pe.close()
     except KeyboardInterrupt:
         pass
-    print "\nRan %d tests\nFailed %d\nTook %d seconds" % (total_run, total_failed, time.time()-start)
+    print (bcolors.ENDC + "\nRan %d tests\n" + bcolors.BOLD + bcolors.FAIL + "Failed %d\n" + bcolors.ENDC + "Took %d seconds") % (
+        total_run, total_failed, time.time()-start)
