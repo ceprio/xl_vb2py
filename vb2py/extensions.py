548aab08dd8c7d0240d1ec43d765a53918607da3
Paul Paterson <ppaterson@gmail.com>
2020-04-13 00:14:37 -0500

    Cleaned up code

--- vb2py/extensions.py
+++ vb2py/extensions.py
@@ -1,16 +1,15 @@
 """Base classes for all plug-ins"""
 
-import glob
 import os
+import re
 import imp
-
 from .utils import rootPath
 from .config import VB2PYConfig
+from . import logger
 
 Config = VB2PYConfig()
-
-from . import logger
 log = logger.getLogger("PlugInLoader")
+
 
 # << Plug-in functions >> (1 of 2)
 def loadAllPlugins():
@@ -33,14 +32,14 @@ def loadAllPlugins():
         #
         for name in dir(m):
             cls = getattr(m, name)
-            #import pdb; pdb.set_trace()
+            # import pdb; pdb.set_trace()
             try:
                 is_plugin = cls.__is_plugin__
             except AttributeError:
                 is_plugin = 0
             if is_plugin:
                 try:
-                    p = cls()               
+                    p = cls()
                     log.info("Added new plug-in: '%s" % p.name)
                     mods.append(p)
                 except Exception as err:
@@ -49,6 +48,8 @@ def loadAllPlugins():
     # Now sort
     mods.sort(key=lambda plugin: plugin.order)
     return mods
+
+
 # << Plug-in functions >> (2 of 2)
 def disableLogging():
     """Disable logging in all plugins"""
@@ -58,15 +59,17 @@ def disableLogging():
     #
     # Now do so for pluging
     BasePlugin.logging_level = 0
+
+
 # -- end -- << Plug-in functions >>
 # << Plug-in classes >> (1 of 4)
 class BasePlugin(object):
     """All plug-ins should inherit from this base class or define __is_plugin__"""
 
-    __is_plugin__ = 1 # Set to true if you want to be loaded plug-in
+    __is_plugin__ = 1  # Set to true if you want to be loaded plug-in
 
-    system_plugin = 0 # True if you are a system plugin
-    __enabled = 1   # If false the plugin will not be called
+    system_plugin = 0  # True if you are a system plugin
+    __enabled = 1  # If false the plugin will not be called
     order = 1000  # Determines order of execution. lower = earlier
 
     logging_level = int(Config["General", "LoggingLevel"])
@@ -83,6 +86,7 @@ class BasePlugin(object):
 
         self.log = logger.getLogger(self.name)
         self.log.setLevel(self.logging_level)
+
     # << BasePlugin methods >> (2 of 6)
     def preProcessVBText(self, text):
         """Process raw VB text prior to any conversion
@@ -92,6 +96,7 @@ class BasePlugin(object):
 
         """
         return text
+
     # << BasePlugin methods >> (3 of 6)
     def postProcessPythonText(self, text):
         """Process Python text following the conversion
@@ -101,21 +106,22 @@ class BasePlugin(object):
 
         """
         return text
+
     # << BasePlugin methods >> (4 of 6)
     def disable(self):
         """Disable the plugin"""
         self.__enabled = 0
+
     # << BasePlugin methods >> (5 of 6)
     def isEnabled(self):
         """Return 1 if plugin is enabled"""
         return self.__enabled
+
     # << BasePlugin methods >> (6 of 6)
     def __cmp__(self, other):
         """Used to allow plugins to be sorted to run in a certain order"""
         return cmp(self.order, other.order)
-    # -- end -- << BasePlugin methods >>
-# << Plug-in classes >> (2 of 4)
-import re
+
 
 class RETextMarkup(BasePlugin):
     """A utility class to apply regular expression based text markup
@@ -129,7 +135,7 @@ class RETextMarkup(BasePlugin):
     """
 
     name = "RETextMarkup"
-    re_flags = 0 # Put re flags here if you need them
+    re_flags = 0  # Put re flags here if you need them
 
     # Define your patterns by assigning to these properties in the sub-class
     pre_process_patterns = ()
@@ -141,12 +147,14 @@ class RETextMarkup(BasePlugin):
         if self.pre_process_patterns:
             self.log.info("Processing pre patterns")
         return self.processText(text, self.pre_process_patterns)
+
     # << RETextMarkup methods >> (2 of 3)
     def postProcessPythonText(self, text):
         """Process Python text following the conversion"""
         if self.post_process_patterns:
             self.log.info("Processing post patterns")
         return self.processText(text, self.post_process_patterns)
+
     # << RETextMarkup methods >> (3 of 3)
     def processText(self, text, patterns):
         """Process the text and mark it up"""
@@ -154,12 +162,11 @@ class RETextMarkup(BasePlugin):
             def doSub(match):
                 self.log.info("Replacing '%s' with %s, %s" % (re_pattern, replace, match.groupdict()))
                 return replace % match.groupdict()
+
             r = re.compile(re_pattern, self.re_flags)
             text = r.sub(doSub, text)
         return text
-    # -- end -- << RETextMarkup methods >>
-# << Plug-in classes >> (3 of 4)
-import re
+
 
 class RenderHookPlugin(BasePlugin):
     """A utility plugin to hook a render method and apply markup after the render
@@ -170,7 +177,7 @@ class RenderHookPlugin(BasePlugin):
     """
 
     name = "RenderHookPlugin"
-    hooked_class_name = None # Name of class should go here
+    hooked_class_name = None  # Name of class should go here
 
     # << RenderHookPlugin methods >> (1 of 2)
     def __init__(self):
@@ -185,27 +192,35 @@ class RenderHookPlugin(BasePlugin):
         from . import parserclasses
         self.hooked_class = getattr(parserclasses, self.hooked_class_name)
         old_render_method = self.hooked_class.renderAsCode
+
         #
         def newRender(obj, indent=0):
             ret = old_render_method(obj, indent)
             return self.addMarkup(indent, ret)
-        #    
+
+        #
         self.hooked_class.renderAsCode = newRender
+
     # << RenderHookPlugin methods >> (2 of 2)
     def addMarkup(self, indent, text):
         """Add markup to the rendered text"""
         return text
     # -- end -- << RenderHookPlugin methods >>
+
+
 # << Plug-in classes >> (4 of 4)
 class SystemPlugin(BasePlugin):
     """Special kind of plug-in which is used by the system and cannot be disabled"""
 
     system_plugin = 1
 
+
 class SystemPluginREPlugin(RETextMarkup):
     """Special kind of plug-in which is used by the system and cannot be disabled"""
 
     system_plugin = 1
+
+
 # -- end -- << Plug-in classes >>
 
 if __name__ == "__main__":
